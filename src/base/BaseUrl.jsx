b6cf9ea97f445848396987f4c5ee800c1c8e7172
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-04-28 11:51:58 +0530

    ` change the baseUrl`

--- src/base/BaseUrl.jsx
+++ src/base/BaseUrl.jsx
@@ -1,3 +1,4 @@
-const BASE_URL = "https://agsdraft.online/app/public";
+// const BASE_URL = "https://agsdraft.online/app/public";
+const BASE_URL = "https://agsdemo.in/v3careapi/public";
 
 export default BASE_URL;
