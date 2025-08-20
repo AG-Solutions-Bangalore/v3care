c7322c6ea6a0afa769e00bad3d068ff4c4f4a3ad
AGS3 <moorthy.chandiran21@gmail.com>
2025-08-20 17:27:19 +0530

    baseurl implementation

--- src/base/BaseUrl.jsx
+++ src/base/BaseUrl.jsx
@@ -1,56 +1,72 @@
-// testing
-// const BASE_URL = "https://agscare.site/crmapi/public";
+// const BASE_URL = "https://v3care.com/crmapi/public";
 // const SUPER_SERVICE_IMAGE_URL =
-//   "https://agscare.site/crmapi/storage/app/public/service_super";
+//   "https://v3care.com/crmapi/storage/app/public/service_super";
 // const SERVICE_IMAGE_URL =
-//   "https://agscare.site/crmapi/storage/app/public/service";
+//   "https://v3care.com/crmapi/storage/app/public/service";
 // const SERVICE_SUB_IMAGE_URL =
-//   "https://agscare.site/crmapi/storage/app/public/service_sub";
+//   "https://v3care.com/crmapi/storage/app/public/service_sub";
 // const NO_IMAGE_URL =
-//   "https://agscare.site/crmapi/storage/app/public/no_image.jpg";
+//   "https://v3care.com/crmapi/storage/app/public/no_image.jpg";
 // const VENDOR_ADAHAR_FRONT_URL =
-//   "https://agscare.site/crmapi/storage/app/public/vendor_aadhar";
+//   "https://v3care.com/crmapi/storage/app/public/vendor_aadhar";
 // const VENDOR_ADAHAR_BACK_URL =
-//   "https://agscare.site/crmapi/storage/app/public/vendor_aadhar";
+//   "https://v3care.com/crmapi/storage/app/public/vendor_aadhar";
 // const VENDOR_GST_URL =
-//   "https://agscare.site/crmapi/storage/app/public/vendor_gst";
+//   "https://v3care.com/crmapi/storage/app/public/vendor_gst";
 // const VENDOR_IMAGE_URL =
-//   "https://agscare.site/crmapi/storage/app/public/vendor_images";
+//   "https://v3care.com/crmapi/storage/app/public/vendor_images";
 // const USER_DOCUMENT =
-//   "https://agscare.site/crmapi/storage/app/public/user_document";
+//   "https://v3care.com/crmapi/storage/app/public/user_document";
 // const NOTIFICATION_IMAGE =
-//   "https://agscare.site/crmapi/storage/app/public/notification_images";
+//   "https://v3care.com/crmapi/storage/app/public/notification_images";
 // const RIGHTSIDEBAR_IMAGE =
-//   "https://agscare.site/crmapi/storage/app/public/service_details";
-// const CLIENTS_IMAGE = "https://agscare.site/crmapi/storage/app/public/clients";
-// const BLOGS_IMAGE = "https://agscare.site/crmapi/storage/app/public/blog";
+//   "https://v3care.com/crmapi/storage/app/public/service_details";
+// const CLIENTS_IMAGE = "https://v3care.com/crmapi/storage/app/public/clients";
+// const BLOGS_IMAGE = "https://v3care.com/crmapi/storage/app/public/blog";
+
+// export {
+//   BASE_URL,
+//   NO_IMAGE_URL,
+//   SERVICE_IMAGE_URL,
+//   SERVICE_SUB_IMAGE_URL,
+//   VENDOR_IMAGE_URL,
+//   VENDOR_GST_URL,
+//   VENDOR_ADAHAR_BACK_URL,
+//   VENDOR_ADAHAR_FRONT_URL,
+//   USER_DOCUMENT,
+//   NOTIFICATION_IMAGE,
+//   SUPER_SERVICE_IMAGE_URL,
+//   RIGHTSIDEBAR_IMAGE,
+//   CLIENTS_IMAGE,
+//   BLOGS_IMAGE,
+// };
 
 // actual url
-const BASE_URL = "https://v3care.com/crmapi/public";
+const BASE_URL = "https://v3care.in/crmapi/public";
 const SUPER_SERVICE_IMAGE_URL =
-  "https://v3care.com/crmapi/storage/app/public/service_super";
+  "https://v3care.in/crmapi/storage/app/public/service_super";
 const SERVICE_IMAGE_URL =
-  "https://v3care.com/crmapi/storage/app/public/service";
+  "https://v3care.in/crmapi/storage/app/public/service";
 const SERVICE_SUB_IMAGE_URL =
-  "https://v3care.com/crmapi/storage/app/public/service_sub";
+  "https://v3care.in/crmapi/storage/app/public/service_sub";
 const NO_IMAGE_URL =
-  "https://v3care.com/crmapi/storage/app/public/no_image.jpg";
+  "https://v3care.in/crmapi/storage/app/public/no_image.jpg";
 const VENDOR_ADAHAR_FRONT_URL =
-  "https://v3care.com/crmapi/storage/app/public/vendor_aadhar";
+  "https://v3care.in/crmapi/storage/app/public/vendor_aadhar";
 const VENDOR_ADAHAR_BACK_URL =
-  "https://v3care.com/crmapi/storage/app/public/vendor_aadhar";
+  "https://v3care.in/crmapi/storage/app/public/vendor_aadhar";
 const VENDOR_GST_URL =
-  "https://v3care.com/crmapi/storage/app/public/vendor_gst";
+  "https://v3care.in/crmapi/storage/app/public/vendor_gst";
 const VENDOR_IMAGE_URL =
-  "https://v3care.com/crmapi/storage/app/public/vendor_images";
+  "https://v3care.in/crmapi/storage/app/public/vendor_images";
 const USER_DOCUMENT =
-  "https://v3care.com/crmapi/storage/app/public/user_document";
+  "https://v3care.in/crmapi/storage/app/public/user_document";
 const NOTIFICATION_IMAGE =
-  "https://v3care.com/crmapi/storage/app/public/notification_images";
+  "https://v3care.in/crmapi/storage/app/public/notification_images";
 const RIGHTSIDEBAR_IMAGE =
-  "https://v3care.com/crmapi/storage/app/public/service_details";
-const CLIENTS_IMAGE = "https://v3care.com/crmapi/storage/app/public/clients";
-const BLOGS_IMAGE = "https://v3care.com/crmapi/storage/app/public/blog";
+  "https://v3care.in/crmapi/storage/app/public/service_details";
+const CLIENTS_IMAGE = "https://v3care.in/crmapi/storage/app/public/clients";
+const BLOGS_IMAGE = "https://v3care.in/crmapi/storage/app/public/blog";
 
 export {
   BASE_URL,
